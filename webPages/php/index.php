8bbf31c5f6b1e88a81676083b8cb384a06875ffe
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-18 16:08:25 -0700

    Add files via upload
    Log in home page.

---
+++ webPages/php/index.php
@@ -0,0 +1,27 @@
+<html>
+
+    <head>
+
+        <title>Log in</title>
+
+    </head>
+
+    <body>
+
+        <?php
+
+            printf("
+                
+                <form>
+                    <input type='text' /><br /><br />
+                    <input type='text' /><br /><br />
+                    <input type='submit' value='Login'>
+                </form>
+
+            ");
+
+        ?>
+
+    </body>
+
+</html>
