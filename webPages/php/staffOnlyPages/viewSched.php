5ebdc2f823ad0a9cf67ebea932b35bec26d563bf
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-18 19:52:41 -0700

    Add files via upload

---
+++ webPages/php/staffOnlyPages/viewSched.php
@@ -0,0 +1,25 @@
+<html>
+    
+    <head>
+        <title>View Schedule</title>
+    </head>
+    
+    <body>
+
+        <?php
+
+            //Verify user
+
+            //Include header
+
+            //Include navbar
+            //Possibly a mobile specific navbar include as well
+
+            //Calendar goes here
+            
+
+        ?>
+
+    </body>
+
+</html>
