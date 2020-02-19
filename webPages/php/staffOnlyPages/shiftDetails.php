04e71d1be6fe8815bc625fad81bf28c2769487be
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-18 20:03:40 -0700

    Add files via upload

---
+++ webPages/php/staffOnlyPages/shiftDetails.php
@@ -0,0 +1,31 @@
+<html>
+    
+    <head>
+
+        <title>Shift Details</title>
+
+    </head>
+
+    <body>
+
+        <?php
+
+            //Verify user
+
+            //Include header
+
+            //Include navbar
+            //Possibly a mobile specific navbar include as well
+
+            //Query db for data of the shift clicked
+            
+            //Display data from query
+
+            //Anchor back to view schedule page this is only a placeholder the proper path will have to be defined according to the directory structure
+            printf("<a href='viewSched.php'></a>");
+
+        ?>
+
+    </body>
+
+</html>
