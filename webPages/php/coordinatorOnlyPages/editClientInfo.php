15b885dee5f7afc9f725810c05459586525fbc77
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 23:14:42 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/editClientInfo.php
@@ -0,0 +1,69 @@
+
+<html>
+
+<head>
+
+    <title>Edit Client Information</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    printf("
+
+            <h1>Edit Client</h1>
+
+            <form action=''>
+
+                ID: <br /><br />
+
+                First Name:
+                    <input type='text' name='' value=''>
+                Last Name:
+                    <input type='text' name='' value=''><br /><br />
+
+                Full Address:
+                    <input type='text' name='' value=''>
+                City:
+                    <input type='text' name='' value=''><br /><br />
+
+                Primary Phone Number:
+                    <input type='text' name='' value=''>
+                Secondary Phone Number:
+                    <input type='text' name='' value=''><br /><br />
+
+                Group Home:
+                    <select name=''>
+                        <option></option>
+                    </select><br /><br />
+
+                Hours Per Month:
+                    <input type='text' name='' value=''><br /><br /><br />
+
+                Guardian:<br /><br />
+
+                    First Name:
+                        <input type='text' name='' value=''>
+                    Last Name:
+                        <input type='text' name='' value=''><br /><br />
+
+                    Phone Number:
+                        <input type='text' name='' value=''><br /><br /><br />
+
+                    Notes:
+                        <textarea name=''>
+                        </textarea><br /><br />
+
+                <input type='submit' value='Submit'>
+
+            </form>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
