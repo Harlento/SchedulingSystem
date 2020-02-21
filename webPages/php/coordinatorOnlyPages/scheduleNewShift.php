2c22e0cb3f3c403ca57d8a7803f5ae7935f6a711
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-21 00:07:20 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/scheduleNewShift.php
@@ -0,0 +1,63 @@
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Schedule New Shift</h1>
+
+            Search for Client:
+                <input type='' name='' value=''><br /><br />
+
+            Client Results:
+                <input type='' name='' value=''><br /><br />
+
+            Department:
+                <input type='' name='' value=''><br /><br />
+
+            Shift Date:
+                <input type='date' name='' value=''><br /><br />
+
+            Start Time:
+                <input type='time' name='' value=''><br /><br />
+
+            End Time:
+                <input type='time' name='' value=''><br /><br />
+
+            Search for staff:
+                <input type='text' name='' value=''><br /><br />
+
+            Staff results:
+                <select name=''>
+                    <option>
+                    </option>
+                </select><br /><br />
+
+            Supervisor:
+                <input type='checkbox' name='' value=''><br /><br />
+
+            Recurring shift:
+                <input type='checkbox' name='' value=''><br /><br />
+
+            Shift Notes:
+                <textarea name=''>
+                </textarea><br /><br />
+
+            <input type='submit' value='Submit'>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
