7bbb6f60d64db6aa52a8af40a12286097012f320
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 23:25:00 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/editGroupHomeInfo.php
@@ -0,0 +1,37 @@
+
+<html>
+
+<head>
+
+    <title>Add Group Home</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    printf("
+
+            <h1>Add Group Home</h1>
+
+            ID: <br /><br />
+
+            Group Home Name:
+                <input type='text' name='' value=''><br /><br />
+
+            Group Home Address:
+                <input type='text' name='' value=''><br /><br />
+
+            Phone Number:
+                <input type='text' name='' value=''><br /><br />
+
+            <input type='submit' value='Submit'>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
