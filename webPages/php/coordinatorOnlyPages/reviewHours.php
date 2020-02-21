dbc923a0033a1fa53df62a647ab99a123a93976a
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-21 00:57:55 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/reviewHours.php
@@ -0,0 +1,24 @@
+
+<html>
+
+<head>
+
+    <title>Review Hours</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Approve Hours</h1>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
