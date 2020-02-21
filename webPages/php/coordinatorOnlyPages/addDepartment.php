80093c01481bb57832094fad3fbec5a6189d2e02
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 23:30:20 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/addDepartment.php
@@ -0,0 +1,36 @@
+
+<html>
+
+<head>
+
+    <title>Add Department</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Add Department</h1>
+
+            Department Code:
+                <input type='text' name='' value=''><br /><br />
+            
+            Department Name:
+                <input type='text' name='' value=''><br /><br />
+
+            Department Description:
+                <textarea>
+                </textarea><br /><br />
+
+            <input type='submit' value='Submit'>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
