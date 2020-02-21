e2fd83e865ba3e239a262f3d3a15b93f174ea720
ThatOneLoneBear <beryonclark@hotmail.com>
2020-02-20 17:15:46 -0700

    Created readme file for directory "Library".

---
+++ webPages/php/Library/valUserType.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+/*  Developer:   Beryon Clark
+ *  File Name:   valUserType.php
+ *  Description: Verifies the user type of any user
+ *  Date Start:  20/02/2020
+ *  Date End:    TBD
+ *  TODO:        -Test functionality
+ */
+
+
+
+?>
