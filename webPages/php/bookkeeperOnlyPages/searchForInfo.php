9e9457d74d01cc6173eb9eca821eff83e8ed9aab
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 21:22:46 -0700

    Add files via upload

---
+++ webPages/php/bookkeeperOnlyPages/searchForInfo.php
@@ -0,0 +1,29 @@
+
+<html>
+
+<head>
+
+    <title>Search for Information</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+            
+            <h1>Search for Information</h1>
+            <form>
+                Search for Staff or Clients:
+                    <input type='text' name='' value=''>
+                <input type='submit' value='Search'>
+            </form>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
