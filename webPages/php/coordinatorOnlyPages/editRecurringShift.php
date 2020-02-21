dd86f419ace39a8274ec6dac1552f6630f5ac341
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-21 00:32:36 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/editRecurringShift.php
@@ -0,0 +1,72 @@
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Edit Recurring Shift</h1>
+
+            Search for Client:
+                <input type='text' name='' value=''><br /><br />
+
+            Client Results:
+                <select>
+                    <option>
+                    </option>
+                </select><br /><br />
+
+            Department:
+                <input type='text' name='' value=''><br /><br />
+
+            Shift Date:
+                <input type='date' name='' value=''><br /><br />
+
+            Scheduled Start:
+                <input type='time' name='' value=''>
+            Scheduled End:
+                <input type='time' name='' value=''><br /><br />
+
+            Claimed Start:
+                <input type='time' name='' value=''>
+            Claimed End:
+                <input type='time' name='' value=''><br /><br />
+
+            Approved Start:
+                <input type='time' name='' value=''>
+            Approved End:
+                <input type='time' name='' value=''><br /><br />
+
+            Search for Staff:
+                <input type='text' name='' value=''><br /><br />
+
+            Staff Results:
+                <select name=''>
+                    <option>
+                    </option>
+                </select><br /><br />
+
+            Supervisor:
+                <input type='checkbox' name='' value=''><br /><br />
+
+            Shift Notes:
+                <textarea>
+                </textarea><br /><br />
+
+            <input type='submit' value='Submit'>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
