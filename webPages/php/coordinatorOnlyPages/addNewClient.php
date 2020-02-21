1d75ec09c1b44c28cfc03be38988d2a44f6c32ed
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 22:58:57 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/addNewClient.php
@@ -0,0 +1,67 @@
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Add New Client</h1>
+
+            <form action=''>
+
+                First Name:
+                    <input type='text' name='' value=''>
+                Last Name:
+                    <input type='text' name='' value=''><br /><br />
+
+                Full Address:
+                    <input type='text' name='' value=''>
+                City:
+                    <input type='text' name='' value=''><br /><br />
+
+                Primary Phone Number:
+                    <input type='text' name='' value=''>
+                Secondary Phone Number:
+                    <input type='text' name='' value=''><br /><br />
+
+                Group Home:
+                    <select name=''>
+                        <option></option>
+                    </select><br /><br />
+
+                Hours Per Month:
+                    <input type='text' name='' value=''><br /><br /><br />
+                
+                Guardian:<br /><br />
+                    
+                    First Name:
+                        <input type='text' name='' value=''>
+                    Last Name:
+                        <input type='text' name='' value=''><br /><br />
+
+                    Phone Number:
+                        <input type='text' name='' value=''><br /><br /><br />
+
+                    Notes:
+                        <textarea name=''>
+                        </textarea><br /><br />
+
+                <input type='submit' value='Submit'>
+                    
+            </form>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
