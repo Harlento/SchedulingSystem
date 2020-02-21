99db95598214ed47f003d10513650bd3180da0f9
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-21 01:16:45 -0700

    Update scheduleRecurringShifts.php

--- webPages/php/coordinatorOnlyPages/scheduleRecurringShifts.php
+++ webPages/php/coordinatorOnlyPages/scheduleRecurringShifts.php
@@ -1,38 +1,37 @@
-
 <html>
 
-<head>
+    <head>
 
-    <title></title>
+        <title>Schedule Recurring Shifts</title>
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <?php
+        <?php
 
-        printf("
+            printf("
 
-            <h1>Schedule Recurring Shifts</h1>
+                <h1>Schedule Recurring Shifts</h1>
 
-            Schedule All Recurring Shifts for Department:
-                <select>
-                    <option>
-                    </option>
-                </select><br /><br />
+                Schedule All Recurring Shifts for Department:
+                    <select>
+                        <option>
+                        </option>
+                    </select><br /><br />
 
-            From Start Date:
-                <input type='date' name='' value=''><br /><br />
+                From Start Date:
+                    <input type='date' name='' value=''><br /><br />
 
-            To End Date:
-                <input type='date' name='' value=''><br /><br />
+                To End Date:
+                    <input type='date' name='' value=''><br /><br />
 
-            <input type='submit' value='Schedule Shifts'>
+                <input type='submit' value='Schedule Shifts'>
 
-        ");
+            ");
 
-    ?>
+        ?>
 
-</body>
+    </body>
 
 </html>
