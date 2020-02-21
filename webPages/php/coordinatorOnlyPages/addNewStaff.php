43ee8faef804d5130d358afb9577fd9b26540644
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-20 22:09:52 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/addNewStaff.php
@@ -0,0 +1,95 @@
+
+<html>
+
+<head>
+
+    <title>Add New Staff Member</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <form action=''>
+
+                First Name:
+                    <input type='text' name='' value=''>
+                Last Name:
+                    <input type='text' name='' value=''><br /><br />
+
+                Full Address:
+                    <input type='text' name='' value=''>
+                City:
+                    <input type='text' name='' value=''><br /><br />
+
+                Primary Phone Number:
+                    <input type='text' name='' value=''>
+                Secondary Phone Number:
+                    <input type='text' name='' value=''><br /><br />
+
+                User Type:
+                    <select>
+                        <option>
+                    </select><br />
+
+                User Name:
+                    <input type='text' name='' value=''>
+                Password:
+                    <input type='text' name='' value=''><br /><br />
+
+                Availability: <br />
+                    Sunday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+                    Monday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                    Tuesday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                     Wednesday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                     Thursday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                     Friday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                     Saturday:
+                        Start:
+                            <input type='time' name='' value=''>
+                        End:
+                            <input type='time' name='' value=''><br /><br />
+
+                <input type='submit' value='Submit'>
+
+            </form>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
