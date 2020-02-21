9d2ad3e39fed517151774b7bde50bc4e97fed81e
Harlento <50353117+Harlento@users.noreply.github.com>
2020-02-21 00:51:47 -0700

    Add files via upload

---
+++ webPages/php/coordinatorOnlyPages/approveHours.php
@@ -0,0 +1,26 @@
+
+<html>
+
+<head>
+
+    <title>Approve Hours</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Approve Hours</h1>
+
+        ");
+
+        //table here
+
+    ?>
+
+</body>
+
+</html>
---
+++ webPages/php/coordinatorOnlyPages/scheduleRecurringShifts.php
@@ -0,0 +1,38 @@
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <?php
+
+        printf("
+
+            <h1>Schedule Recurring Shifts</h1>
+
+            Schedule All Recurring Shifts for Department:
+                <select>
+                    <option>
+                    </option>
+                </select><br /><br />
+
+            From Start Date:
+                <input type='date' name='' value=''><br /><br />
+
+            To End Date:
+                <input type='date' name='' value=''><br /><br />
+
+            <input type='submit' value='Schedule Shifts'>
+
+        ");
+
+    ?>
+
+</body>
+
+</html>
