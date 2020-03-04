044cac09dad557153a5faf39452881d2efe38487
ThatOneLoneBear <beryonclark@hotmail.com>
2020-03-04 13:32:55 -0700

    Minor updates. Reorganization.

--- webPages/php/Library/inputFilter.php
+++ webPages/php/Library/inputFilter.php
@@ -1,16 +1,11 @@
 <?php
 /*  Developer:   Beryon Clark
  *  File Name:   inputFilter.php
- *  Description: Extends multiple functions to filter/validate input.
+ *  Description: Extends multiple functions to validate input.
  *  Date Start:  26/02/2020
  *  Date End:    TBD
- *  TODO:
  */
 
-function sanitizeInput()
-{
-
-}
 function validateInput()
 {
 
