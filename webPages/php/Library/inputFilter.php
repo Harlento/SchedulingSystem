28b10bbf8a68691960714257d19b32223f46a654
ThatOneLoneBear <beryonclark@hotmail.com>
2020-03-06 14:06:04 -0700

    Minor updates. Reorganization.

--- webPages/php/Library/inputFilter.php
+++ webPages/php/Library/inputFilter.php
@@ -6,9 +6,42 @@
  *  Date End:    TBD
  */
 
-function validateInput()
+function validateInput($str, $type)
 {
-
+// Date format being used is yyyy-mm-dd
+    $filtString = filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+    switch ($type)
+    {
+        case "date":
+        {
+            $dateArr  = explode('-', $filtString);
+            if (count($dateArr) == 3) {
+                if (checkdate($dateArr[0], $dateArr[1], $dateArr[2])) {
+                    // valid date ...
+                }
+                else {
+                    // problem with dates ...
+                }
+            }
+            else {
+                // problem with input ...
+            }
+        }
+        case "string":
+        {
+            return $str;
+        }
+        case "email":
+        {
+            $emailString = filter_var($filtString, FILTER_VALIDATE_EMAIL);
+            return $emailString;
+        }
+        case "phone":
+        {
+            filter_var($filtString, FILTER_VALIDATE_REGEXP);
+            return $str;
+        }
+    }
 }
 
 ?>
