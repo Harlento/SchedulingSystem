ac3c5d2641cf0c436d20e00a6f76dc83c7d79dc5
Rung-young <56895789+Rung-young@users.noreply.github.com>
2020-03-11 15:29:16 -0600

    Update content box.php

--- webPages/php/content box.php
+++ webPages/php/content box.php
@@ -1,45 +1,31 @@
 <?php
-echo'<!DOCTYPE html>
+echo'
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Table</title>
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/table.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
-<section class="container-fluid">
-    <section class="row justify-content-center">
-        <section class="col-12 col-lg-6 col-lg-3">
-            <form class="form-con">
-                <form>
-
-                    <div>';
-
-//Put PHP here
-
-
-        
-
-	echo '                   </div>
-                    <div>
-
-                        <div class=\"form-check\">
-                            <input class=\"form-check-input\" type=\"checkbox\" value=\"\" id=\"defaultCheck1\">
-                            <label class=\"form-check-label\" for=\"defaultCheck1\">
-                                :By checking this box, I certify that this timesheet is complete and accurately reflects my time and effort
-                            </label>
-                        </div>
-                    </div>
-                    <button class=\"btn btn-primary\" type=\"submit\">Submit</button>
-                </form>
-            </form>
-        </section>
-    </section>
-</section>
-</body>
-</html>
+<div class="row justify-content-center">
+<form class="form-con">
+    <form>
 ';
 
-?>
+//Put PHP CODE HERE
+
+
+
+echo'
+            </div>
+    </form>
+</form>
+</div>
+</body>
+</html>
+<?
+    ';
