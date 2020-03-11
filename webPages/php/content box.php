89790e217b248a3cd4bb535626d7689d5bfedca1
Rung-young <56895789+Rung-young@users.noreply.github.com>
2020-03-11 13:31:44 -0600

    Add files via upload

---
+++ webPages/php/content box.php
@@ -0,0 +1,45 @@
+<?php
+echo'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/table.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<section class="container-fluid">
+    <section class="row justify-content-center">
+        <section class="col-12 col-lg-6 col-lg-3">
+            <form class="form-con">
+                <form>
+
+                    <div>';
+
+//Put PHP here
+
+
+        
+
+	echo '                   </div>
+                    <div>
+
+                        <div class=\"form-check\">
+                            <input class=\"form-check-input\" type=\"checkbox\" value=\"\" id=\"defaultCheck1\">
+                            <label class=\"form-check-label\" for=\"defaultCheck1\">
+                                :By checking this box, I certify that this timesheet is complete and accurately reflects my time and effort
+                            </label>
+                        </div>
+                    </div>
+                    <button class=\"btn btn-primary\" type=\"submit\">Submit</button>
+                </form>
+            </form>
+        </section>
+    </section>
+</section>
+</body>
+</html>
+';
+
+?>
